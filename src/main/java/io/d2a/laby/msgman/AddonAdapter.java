5e5ff571f35a07acc43ea4e575d14da51e2f48ac
darmiel <71837281+darmiel@users.noreply.github.com>
2021-07-18 11:06:30 +0200

    This commit doesn't really deserve its own message. A few things from the template were adapted.

---
+++ src/main/java/io/d2a/laby/msgman/AddonAdapter.java
@@ -0,0 +1,24 @@
+package io.d2a.laby.msgman;
+
+import java.util.List;
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+public class AddonAdapter extends LabyModAddon {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void loadConfig() {
+
+  }
+
+  @Override
+  protected void fillSettings(final List<SettingsElement> list) {
+
+  }
+
+}
---
+++ src/main/java/io/d2a/laby/msgman/MessageManager.java
@@ -0,0 +1,10 @@
+package io.d2a.laby.msgman;
+
+public class MessageManager extends AddonAdapter {
+
+  @Override
+  public void onEnable() {
+    System.out.println("It works! (or does it?)");
+  }
+
+}
